5a800b8e53217b3e30cf4e0982bf4d5cbc3f8e25
psmishenko <psmishenko@gmail.com>
2022-05-18 16:03:35 +0300

    Fix rubocop issues

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,30 +1,28 @@
+# frozen_string_literal: true
+
 class BoardsController < ApplicationController
   before_action :authenticate_user!
-  before_action :current_board, only: [:show, :edit, :update, :destroy]
+  before_action :find_board, except: %i[index new create]
 
   def index
     @boards = Board.all
   end
 
-  def show
-
-  end
+  def show; end
 
   def new
     @board = Board.new
   end
 
   def create
-    board = Board.create(user_id: current_user.id, 
-      name: board_params['name'], 
-      description: board_params['description'])
+    board = Board.create!(user_id: current_user.id,
+                          name: board_params['name'],
+                          description: board_params['description'])
 
     redirect_to board_path(board)
   end
 
-  def edit
-
-  end
+  def edit; end
 
   def update
     @board.update(board_params)
@@ -41,11 +39,12 @@ class BoardsController < ApplicationController
   private
 
   def board_params
-    params.require(:board).permit(:name, :description)
+    params.require(:board)
+          .permit(:name,
+                  :description)
   end
 
-  def current_board
+  def find_board
     @board = Board.find(params[:id])
   end
-
 end
