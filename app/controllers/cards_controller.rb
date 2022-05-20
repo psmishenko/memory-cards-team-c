3ae587777f9472b0ce66646ddc6e18d33607a1ea
psmishenko <psmishenko@gmail.com>
2022-05-20 12:43:14 +0300

    Add CRUD operations for cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,13 +1,45 @@
-class CardsController < ApplicationController
-  def index
-  end
+# frozen_string_literal: true
 
-  def show
+class CardsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :current_card, except: %i[index new create]
+
+  def index
+    @cards = Card.all
   end
 
   def new
+    @card = Card.new
   end
 
-  def edit
+  def create
+    card = Card.create!(board_id: 1,
+                        question: card_params['question'],
+                        answer: card_params['answer'])
+
+    redirect_to cards_path(card)
+  end
+
+  def update
+    @card.update(card_params)
+
+    redirect_to card_path(@card)
+  end
+
+  def destroy
+    @card.destroy
+
+    redirect_to cards_path
+  end
+
+  private
+
+  def card_params
+    params.require(:card)
+          .permit(:question, :answer)
+  end
+
+  def current_card
+    @card = Card.find(params[:id])
   end
 end
