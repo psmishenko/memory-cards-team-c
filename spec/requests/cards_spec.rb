e960bf24d0336a9b4fe3ed63875f73335e65b126
DmitryS1989 <movs1989@gmail.com>
2022-06-01 09:22:43 +0300

    fix specs

--- spec/requests/cards_spec.rb
+++ spec/requests/cards_spec.rb
@@ -4,30 +4,38 @@ require 'rails_helper'
 
 RSpec.describe 'Cards', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/cards/index'
-      expect(response).to have_http_status(:success)
+    context 'when the user is not logged in' do
+      it 'returns redirect' do
+        get '/cards/index'
+        expect(response).to have_http_status(:redirect)
+      end
     end
   end
 
   describe 'GET /show' do
-    it 'returns http success' do
-      get '/cards/show'
-      expect(response).to have_http_status(:success)
+    context 'when the user is not logged in' do
+      it 'returns redirect' do
+        get '/cards/show'
+        expect(response).to have_http_status(:redirect)
+      end
     end
   end
 
   describe 'GET /new' do
-    it 'returns http success' do
-      get '/cards/new'
-      expect(response).to have_http_status(:success)
+    context 'when the user is not logged in' do
+      it 'returns redirect' do
+        get '/cards/new'
+        expect(response).to have_http_status(:redirect)
+      end
     end
   end
 
   describe 'GET /edit' do
-    it 'returns http success' do
-      get '/cards/edit'
-      expect(response).to have_http_status(:success)
+    context 'when the user is not logged in' do
+      it 'returns redirect' do
+        get '/cards/edit'
+        expect(response).to have_http_status(:redirect)
+      end
     end
   end
 end
