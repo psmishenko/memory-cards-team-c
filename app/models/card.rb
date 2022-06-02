7ba65dcb30cf33cd1b0a67841d4cc2c11a180a09
psmishenko <psmishenko@gmail.com>
2022-06-02 14:36:50 +0300

    Add length validation to card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,5 +2,6 @@
 
 class Card < ApplicationRecord
   belongs_to :board
-  validates :question, :answer, presence: true
+  validates :question, presence: { message: "Question is required"}, length: { maximum: 200, message: "%{count} characters is the maximum allowed" } 
+  validates :answer, presence: { message: "Answer is required"}
 end
