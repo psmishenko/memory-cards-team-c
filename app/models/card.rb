421864e7393d27af5f97256edf69d9b15ae98146
psmishenko <psmishenko@gmail.com>
2022-06-02 18:57:12 +0300

    Add card model tests

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,6 +2,7 @@
 
 class Card < ApplicationRecord
   belongs_to :board
-  validates :question, presence: { message: "Question is required"}, length: { maximum: 200, message: "%{count} characters is the maximum allowed" } 
-  validates :answer, presence: { message: "Answer is required"}
+  validates :question, presence: { message: 'Question is required' },
+                       length: { maximum: 200, message: '%{count} characters is the maximum allowed' }
+  validates :answer, presence: { message: 'Answer is required' }
 end
