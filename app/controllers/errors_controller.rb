c30f7eaacc449688370643cc3c19bd03ec07bee6
psmishenko <psmishenko@gmail.com>
2022-06-10 16:37:12 +0300

    Fix rubocop issues

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
 class ErrorsController < ApplicationController
-  skip_before_action :authenticate_user!, :raise => false
+  skip_before_action :authenticate_user!, raise: false
 
   def not_found
     respond_to do |format|
-      format.html { render status: 404 }
+      format.html { render status: :not_found }
     end
   end
 
   def unacceptable
     respond_to do |format|
-      format.html { render status: 422 }
+      format.html { render status: :unprocessable_entity }
     end
   end
 
   def internal_error
     respond_to do |format|
-      format.html { render status: 500 }
+      format.html { render status: :internal_server_error }
     end
   end
 end
